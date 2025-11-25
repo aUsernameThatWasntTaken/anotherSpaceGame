46c6a3ad5e94206c071e041ab6f57e516c7da5fd
aUsernameThatWasntTaken <maxymilianbyzdra@gmail.com>
2025-11-25 20:04:27 +0000

    added rockets to launch

--- gameLogic.py
+++ gameLogic.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import saves
 import asyncio
 from time import time
@@ -9,6 +10,7 @@ class World:
         # this can be used later if changes are needed to structure handling
         # self.infrastructure = dict([(body, [structure for structure in structures]) for body,structures in jsonDict["infrastructure"].items()])
         self.infrastructure = jsonDict["infrastructure"]
+        self.vABSpeed = sum([item["level"] for item in self.infrastructure["earth"] if item["type"]=="VAB"])
 
 defaultSave = {
     "money":1_000_000,
@@ -20,9 +22,16 @@ defaultSave = {
     }
 }
 
+class Rockets(Enum):
+    suborbital = 0 
+
+# in seconds with one Lvl1 VAB:
+rocketBuildTime = {Rockets.suborbital:10}
+
 class GameHandler:
     """Contains code to handle commands and to init the asyncHandler"""
     def __init__(self, saveFilename):
+        self.selectedRocket = Rockets.suborbital
         self.world = World(saves.load(saveFilename, defaultSave))
         self.lastTick = time()
         self.asyncHandler = asyncHandling.handler(self.tick, self.handleInput)
@@ -37,8 +46,10 @@ class GameHandler:
         deltaT=startTime-self.lastTick
         self.lastTick = startTime
     
-    def launchRocket(self):
-        pass # I have not thought this game through at all, and yet here I am: possibly closer that I have ever been to completion
+    async def launchRocket(self):
+        await asyncio.sleep(rocketBuildTime[self.selectedRocket]/self.world.vABSpeed)
+        await asyncio.sleep(10)
+        print("rocket launched")
 
     def handleInput(self, command):
         match command:
