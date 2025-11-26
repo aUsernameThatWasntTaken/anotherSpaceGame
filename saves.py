b29f5264f5a57bc7ee7c695476e0f8c0302cd86c
aUsernameThatWasntTaken <maxymilianbyzdra@gmail.com>
2025-11-26 20:12:57 +0000

    Moved FileNotFoundError handling to launcher for better control with other launchers

--- saves.py
+++ saves.py
@@ -1,8 +1,5 @@
 import json
 
 def load(fileName, default):
-    try:
-        with open(fileName) as f:
-            return json.load(f)
-    except json.JSONDecodeError:
-        return default
+    with open(fileName) as f:
+        return json.load(f)
