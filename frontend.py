65b7f8607fdf5287d94d5011c349e37d2ba292e1
Soissante Neuf <pathansaahil0@gmail.com>
2025-11-28 17:26:08 +0000

    i reuqest to merge

---
+++ frontend.py
@@ -0,0 +1,73 @@
+import pygame
+import pygame_gui
+import os
+
+SCREEN_W = 1280
+SCREEN_H = 720
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_W,SCREEN_H))
+manager = pygame_gui.UIManager((SCREEN_W,SCREEN_H), theme_path="theme.json")
+running = True
+
+# helpers
+def scale_to_offset(scale_size, width_height):
+    if width_height == "width":
+        return (scale_size*SCREEN_W)
+    elif width_height == "height":
+        return (scale_size*SCREEN_H)
+    else:
+        raise Exception('Incorrect Parameter. Use "width" or "height".')
+
+def centered_rect(center_x_scale, center_y_scale, width_scale, height_scale):
+    width = scale_to_offset(width_scale, "width")
+    height = scale_to_offset(height_scale, "height")
+    center_x = scale_to_offset(center_x_scale, "width")
+    center_y = scale_to_offset(center_y_scale, "height")
+    
+    top_left_x = center_x - width/2
+    top_left_y = center_y - height/2
+    
+    return pygame.Rect((top_left_x, top_left_y), (width, height))
+
+# UI
+launch_button = pygame_gui.elements.UIButton(relative_rect=centered_rect(0.15, 0.9, 0.2, 0.1),
+                                             text='PREPARE PAYLOAD',
+                                             manager=manager)
+
+# bottom right buttons in a horizontal line
+build_button = pygame_gui.elements.UIButton(relative_rect=centered_rect(0.75, 0.9, 0.1, 0.1),
+                                            text='BUILD',
+                                            manager=manager,
+                                            object_id='#build_btn')
+research_button = pygame_gui.elements.UIButton(relative_rect=centered_rect(0.875, 0.9, 0.1, 0.1),
+                                               text='RESEARCH',
+                                               manager=manager,
+                                               object_id='#research_btn')
+payload_button = pygame_gui.elements.UIButton(relative_rect=centered_rect(0.625, 0.9, 0.1, 0.1),
+                                             text='CIV',
+                                             manager=manager,
+                                             object_id='#payload_btn')
+
+# things we need
+
+# launchpad INFO
+# tech tree & research
+# cargo selection (COMMERCIAL/SCIENTIFIC/SPACESHIP)
+# money!
+
+clock = pygame.time.Clock()
+
+while running:
+    dt = clock.tick(60)/1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        manager.process_events(event)
+    
+    manager.update(dt)
+    manager.draw_ui(screen)
+    pygame.display.update()
+
+pygame.quit()
