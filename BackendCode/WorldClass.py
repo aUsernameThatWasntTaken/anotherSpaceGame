1b1b99b14b11e5129b8b6d8c7fe7c76e7332a6b5
aUsernameThatWasntTaken <maxymilianbyzdra@gmail.com>
2025-11-30 13:41:31 +0000

    Extracted some code

--- BackendCode/WorldClass.py
+++ BackendCode/WorldClass.py
@@ -57,10 +57,7 @@ class World:
     async def handleVAB(self):
         try:
             while True:
-                toBuild = await self.queues.build.get()
-                if toBuild == "rocket":
-                    await asyncio.sleep(self.rocketBuildTime)
-                    self.queues.launch.put_nowait("rocket")
+                self.handleOneBuild()
         except asyncio.CancelledError:
             # clean shutdown if cancelled
             return
@@ -68,20 +65,28 @@ class World:
     async def handleLaunchPad(self):
         try:
             while True:
-
-                self.padStats.awaitingRocket +=1
-                rocket = await self.queues.launch.get()
-                self.padStats.awaitingRocket -=1
-
-                self.padStats.awaitingPayload +=1
-                payload = await self.queues.payload.get()
-                self.padStats.awaitingPayload -=1
-
-                if True:
-                    self.padStats.launching +=1
-                    await asyncio.sleep(5)
-                    self.padStats.launching -=1
-                    self.rocketsLaunched += 1
+                await self.handleOneLaunch()
         except asyncio.CancelledError:
             # clean shutdown if cancelled
             return
+
+    async def handleOneLaunch(self):
+        self.padStats.awaitingRocket +=1
+        rocket = await self.queues.launch.get()
+        self.padStats.awaitingRocket -=1
+
+        self.padStats.awaitingPayload +=1
+        payload = await self.queues.payload.get()
+        self.padStats.awaitingPayload -=1
+
+        if True: # wait, what?
+            self.padStats.launching +=1
+            await asyncio.sleep(5)
+            self.padStats.launching -=1
+            self.rocketsLaunched += 1
+    
+    async def handleOneBuild(self):
+        toBuild = await self.queues.build.get()
+        if toBuild == "rocket":
+            await asyncio.sleep(self.rocketBuildTime)
+            self.queues.launch.put_nowait("rocket")
